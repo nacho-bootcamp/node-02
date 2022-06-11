95c1b2f4fa01c375004b2fef4be0cadecc0144a2
nacho-bootcamp <ignaciocardozoxd12@gmail.com>
2022-06-11 13:33:14 -0300

    vercel-deploy

---
+++ vercel-ts-api/api/index.ts
@@ -0,0 +1,8 @@
+import { Request, Response } from "express";
+
+export default async (req: Request, res: Response) => {
+  res.json({ message: "Hello guys. Welcome to Vercel" });
+};
+
+
+// query siempre llega como string
